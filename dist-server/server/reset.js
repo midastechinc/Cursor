b2d1e8748addb4ace790effb77262cfe637654b8
midastechinc <ali@midastech.ca>
2026-03-31 16:02:44 -0400

    Clean repo for GitHub

--- dist-server/server/reset.js
+++
@@ -1,7 +0,0 @@
-import { initializeDatabase, resetDatabase } from "./db/database.js";
-const main = async () => {
-    await initializeDatabase();
-    resetDatabase();
-    console.log("Payroll database reset to seeded defaults.");
-};
-void main();
